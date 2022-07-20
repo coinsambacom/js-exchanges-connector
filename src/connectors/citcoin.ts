005a86e0f46b619f83405764406580ee0fbf9a8d
Gustavo <gustavo@obex.pw>
2022-07-20 19:50:31 -0300

    fix citcoin

--- src/connectors/citcoin.ts
+++ src/connectors/citcoin.ts
@@ -2,8 +2,26 @@ import {
   Exchange,
   IExchangeImplementationConstructorArgs,
 } from "../interfaces/exchange";
-import { ITicker } from "../types/common";
+import { IOrderbookOrder, ITicker } from "../types/common";
 
+interface ICitcoinTickerRes {
+  date: string;
+  high: string;
+  low: string;
+  open: string;
+  close: string;
+  volume: number;
+}
+
+interface ICitcoinOrder {
+  btc: string;
+  btc_price: string;
+}
+
+interface ICitcoinOrderbookRes {
+  ask: ICitcoinOrder[];
+  bid: ICitcoinOrder[];
+}
 export class citcoin<T> extends Exchange<T> {
   constructor(args?: IExchangeImplementationConstructorArgs<T>) {
     super({
@@ -15,36 +33,34 @@ export class citcoin<T> extends Exchange<T> {
   }
 
   async getTicker(base: string, quote: string): Promise<ITicker> {
-    const res = await this.fetch(
-      `${this.baseUrl}/${base.toLowerCase()}/ticker`,
+    const res = await this.fetch<ICitcoinTickerRes>(
+      `${this.baseUrl}/${base.toLowerCase()}/ticker/`,
     );
 
     return {
       exchangeId: this.id,
       base,
       quote,
-      last: res.close,
-      ask: res.close,
-      bid: res.close,
-      vol: res.vol,
+      last: Number(res.close),
+      ask: Number(res.close),
+      bid: Number(res.close),
+      vol: res.volume,
     };
+  }
+
+  private parseOrder({ btc, btc_price }: ICitcoinOrder): IOrderbookOrder {
+    return { price: Number(btc_price), amount: Number(btc) };
   }
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async getBook(base: string, quote: string) {
-    const res = await this.fetch(
-      `${this.baseUrl}/${base.toLowerCase()}/orderbook`,
+    const res = await this.fetch<ICitcoinOrderbookRes>(
+      `${this.baseUrl}/${base.toLowerCase()}/orderbook/`,
     );
 
     return {
-      asks: res.asks.map((o: { btc_price: string; btc: string }) => ({
-        price: Number(o.btc_price),
-        amount: Number(o.btc),
-      })),
-      bids: res.bids.map((o: { btc_price: string; btc: string }) => ({
-        price: Number(o.btc_price),
-        amount: Number(o.btc),
-      })),
+      asks: res.ask.map(this.parseOrder),
+      bids: res.bid.map(this.parseOrder),
     };
   }
 }
