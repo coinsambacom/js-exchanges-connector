e000467dac080a51b12fe214543593b3aaf5d127
Gustavo <gustavo@obex.pw>
2022-11-17 19:25:14 -0300

    added brasil bitcoin otc

---
+++ src/connectors/brasilbitcoin_otc.ts
@@ -0,0 +1,46 @@
+import {
+  Exchange,
+  IExchangeImplementationConstructorArgs,
+} from "../interfaces/exchange";
+import { ITicker } from "../types/common";
+
+interface IBrasilBitcoinOtcTicker {
+  last: number;
+  max: string;
+  min: string;
+  buy: number;
+  sell: number;
+  open: string;
+  vol: number;
+  trade: number;
+  trades: number;
+  vwap: number;
+  money: number;
+}
+
+export class brasilbitcoin_otc<T> extends Exchange<T> {
+  constructor(args?: IExchangeImplementationConstructorArgs<T>) {
+    super({
+      id: "brasilbitcoin_otc",
+      baseUrl: "https://brasilbitcoin.com.br/API",
+      opts: args?.opts,
+      limiter: args?.limiter,
+    });
+  }
+
+  async getTicker(base: string, quote: string): Promise<ITicker> {
+    const res = await this.fetch<IBrasilBitcoinOtcTicker>(
+      `${this.baseUrl}/otc/prices/${base}`,
+    );
+
+    return {
+      exchangeId: this.id,
+      base,
+      quote,
+      last: res.last,
+      ask: res.sell,
+      bid: res.buy,
+      vol: res.vol,
+    };
+  }
+}
