c7fdb0974acd0f08b5999dd90220b37acfea2b21
Gustavo <gustavo@obex.pw>
2023-08-10 01:21:17 -0300

    use tsc

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export * as exchanges from "./exchanges";
-export * as types from "./types";
+export * from "./interfaces/exchange";
+export * as DTOs from "./utils/DTOs";
+export * as ConnectorErrors from "./utils/ConnectorError";
--- src/types.ts
+++
@@ -1,3 +0,0 @@
-export * from "./interfaces/exchange";
-export * from "./utils/DTOs";
-export * from "./utils/ConnectorError";
